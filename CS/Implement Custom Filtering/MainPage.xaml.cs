4bba8584372ae21bdad4d40df6a05310349477a8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Implement Custom Filtering/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+
+namespace Implement_Custom_Filtering {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = new object[] { "A", "B", "C", "D", "E", "F", "G", "H" };
+        }
+        private void grid_CustomRowFilter(object sender, RowFilterEventArgs e) {
+            if(chkHideOdd.IsChecked.Value && e.ListSourceRowIndex % 2 == 1)
+                e.Visible = false;
+            if(chkHideEven.IsChecked.Value && e.ListSourceRowIndex % 2 == 0)
+                e.Visible = false;
+            e.Handled = !e.Visible ? true : false;
+        }
+
+        private void chk_CheckedOrUnchecked(object sender, RoutedEventArgs e) {
+            grid.RefreshData();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# DXGrid - How to Implement Custom Filtering
+
+
+<p>This example demonstrates how to hide even or odd rows.</p>
+
+<br/>
+
+
